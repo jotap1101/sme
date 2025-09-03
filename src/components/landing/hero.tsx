01f4640203b6e86a5aa8dc16ed0f8297ae5189b9
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-09-03 13:22:53 -0300

    fix: add line break in Hero component heading for better readability

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -8,6 +8,7 @@ export default function Hero() {
       <div className="container px-4 text-center md:px-6">
         <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
           Impulsionando Carreiras nas{" "}
+          <br />
           <span className="text-primary">Salas Mineiras</span>
         </h1>
         <p className="text-muted-foreground mx-auto mt-6 max-w-[700px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
